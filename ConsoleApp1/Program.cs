d88746941eaa03f9818a90ca9dda5bd72eaa6a1a
SHAIMA ALSHAMMARY <Shaimacs@yahoo.com>
2021-07-19 13:34:01 +0300

    Upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            StreamWriter file = new StreamWriter(@"D:\NET\Phase 1\Assesment1\SDA-Assesment1\filename.txt");
+            Console.WriteLine("Enter ID");
+            int id = int.Parse(Console.ReadLine());
+            file.WriteLine(id);
+            Console.WriteLine("Enter Name");
+            string name = Console.ReadLine();
+            file.WriteLine(name);
+            Console.WriteLine("Class Name");
+            string _class = Console.ReadLine();
+            file.WriteLine(_class);
+            Console.WriteLine("Enter Section Name");
+            string section = Console.ReadLine();
+            file.WriteLine(section);
+            file.Close();
+
+            StreamReader reader = new StreamReader(@"D:\NET\Phase 1\Assesment1\SDA-Assesment1\filename.txt");
+            string readedData = reader.ReadToEnd();
+            reader.Close();
+
+            StreamWriter writer = new StreamWriter(@"D:\NET\Phase 1\Assesment1\SDA-Assesment1\filename.txt", false);
+            writer.Write(readedData);
+            writer.Close();
+        }
+        }
+}
--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# SDA-Assesment1
-
-Technolgies Used: 
+### SDA-Assesment1
+<br>
+## Technolgies Used: 
 C# Console Application. 
+
+<br>
+<br>
+
+## User Stories:
+- As A Teacher I shoud be able to store, retrieve and update my data. 
+<br>
+<br>
+
+## WireFrames
+<img src = "D:\NET\Phase 1\Assesment1\1.png" > 
+<img src = "D:\NET\Phase 1\Assesment1\2.png" > 
